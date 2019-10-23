b4ca3276c2ffebfd54f1b817fa9a837bda95e36e
Lin Tian <lintian@biag-w04.cs.unc.edu>
2019-10-23 16:21:43 -0400

    Added two functions: 1.projection function 2.module to present registered CT and sDCT image

---
+++ show_in_mermaid.py
@@ -0,0 +1,155 @@
+from mermaid import viewers
+import mermaid
+import numpy as np
+import matplotlib.pyplot as plt
+import pydicom as dicom
+import os
+import torch
+from mermaid import image_sampling
+
+def plot_grid(ax, gridx,gridy, **kwargs):
+    for i in range(gridx.shape[0]):
+        ax.plot(gridx[i,:], gridy[i,:], **kwargs)
+    for i in range(gridx.shape[1]):
+        ax.plot(gridx[:,i], gridy[:,i], **kwargs)
+
+class ImageViewer3D_Sliced_Grids(viewers.ImageViewer3D_Sliced):
+    """
+    Specialization of 3D sliced viewer to also display contours
+    """
+    def __init__(self, ax, data, phi, sliceDim, textStr='Slice', showColorbar=False):
+        """
+        Constructor
+        :param ax: axis
+        :param data: data (image array, XxYxZ)
+        :param phi: map (dimxXxYxZ)
+        :param sliceDim: slice dimension
+        :param textStr: title string
+        :param showColorbar: (bool) show colorbar
+        """
+        self.phi = phi
+        self.d, self.w, self.h = data.shape
+        """map"""
+        super(ImageViewer3D_Sliced_Grids,self).__init__(ax,data, sliceDim, textStr, showColorbar)
+
+    def get_phi_slice_at_dimension(self,index):
+        """
+        Get map (based on which we can draw contours) at a particular slice index
+        
+        :param index: slice index 
+        :return: returns the map at this slice index
+        """
+        # slicing a along a given dimension at index, index
+        slc = [slice(None)] * len(self.phi.shape)
+        slc[self.sliceDim+1] = slice(index, index+1)
+        
+        return (self.phi[tuple(slc)]).squeeze()
+
+    def show_grids(self):
+        """
+        display the contours for a particular slice
+        """
+        plt.sca(self.ax)
+        phiSliced = self.get_phi_slice_at_dimension(self.index)
+        for d in range(0,self.sliceDim):
+            plt.contour(phiSliced[d,:,:], np.linspace(0,self.data.shape[d],20),colors='lightgray',linestyles='solid', linewidths=0.7)
+        for d in range(self.sliceDim+1,3):
+            plt.contour(phiSliced[d,:,:], np.linspace(0,self.data.shape[d],20),colors='lightgray',linestyles='solid', linewidths=0.7)
+
+    def previous_slice(self):
+        """
+        display previous slice
+        """
+        super(ImageViewer3D_Sliced_Grids,self).previous_slice()
+        self.show_grids()
+
+    def next_slice(self):
+        """
+        display next slice
+        """
+        super(ImageViewer3D_Sliced_Grids,self).next_slice()
+        self.show_grids()
+
+    def set_synchronize(self, index):
+        """
+        set slice to a particular index (to synchronize views)
+
+        :param index: slice index 
+        """
+        super(ImageViewer3D_Sliced_Grids,self).set_synchronize(index)
+        self.show_grids()
+
+    def show(self):
+        """
+        Show the image with contours overlaid
+        """
+        super(ImageViewer3D_Sliced_Grids,self).show()
+        self.show_grids()
+
+
+I0_file = "./data/input_noMask_large.npy"
+disp_file = "./data/disp_noMask_large.npy"
+warped_file = "./data/warped_noMask_large.npy"
+sDT_dicom = "../../Data/Raw/DICOMforMN/DICOM/S00002/SER00001"
+SHOW_SDT = True
+
+
+I0 = np.load(I0_file)[0,0]
+phi = np.load(disp_file)[0]
+warped = np.load(warped_file)[0,0]
+d, w, h = I0.shape
+
+if SHOW_SDT:
+    file_list = os.listdir(sDT_dicom)
+    file_list.sort()
+    case  = [dicom.read_file(sDT_dicom + '/' + s) for s in file_list]
+    image = np.stack([s.pixel_array for s in case])
+    image = np.transpose(image, (1,0,2))
+    image = image[:2100,:,:]
+    image = image.astype(np.float32)
+    I1 = torch.from_numpy(image).unsqueeze(0).unsqueeze(0).to(torch.device("cuda"))
+    spacing = np.array([1., 1., 1.])
+    sampler = image_sampling.ResampleImage()
+    sdt, spacing = sampler.downsample_image_by_factor(I1, spacing, scalingFactor=0.48)
+    sdt = sdt.detach().cpu().numpy()[0,0]
+
+fig,ax = plt.subplots(3,3)
+
+plt.setp(plt.gcf(), 'facecolor', 'white')
+plt.style.use('bmh')
+
+ivx = viewers.ImageViewer3D_Sliced( ax[0,0], I0, 0, 'Z slice')
+ivy = viewers.ImageViewer3D_Sliced( ax[0,1], I0, 1, 'X slice')
+ivz = viewers.ImageViewer3D_Sliced( ax[0,2], I0, 2, 'Y slice')
+
+warped_ivx = viewers.ImageViewer3D_Sliced( ax[1,0], warped, 0, 'Z slice')
+warped_ivy = viewers.ImageViewer3D_Sliced( ax[1,1], warped, 1, 'X slice')
+warped_ivz = viewers.ImageViewer3D_Sliced( ax[1,2], warped, 2, 'Y slice')
+
+if SHOW_SDT:
+    warped_ivx_grids = viewers.ImageViewer3D_Sliced( ax[2,0], sdt, 0, 'Z slice')
+    warped_ivy_grids = viewers.ImageViewer3D_Sliced( ax[2,1], sdt, 1, 'X slice')
+    warped_ivz_grids = viewers.ImageViewer3D_Sliced( ax[2,2], sdt, 2, 'Y slice')
+else:
+    warped_ivx_grids = ImageViewer3D_Sliced_Grids( ax[2,0], warped, phi, 0, 'Z slice')
+    warped_ivy_grids = ImageViewer3D_Sliced_Grids( ax[2,1], warped, phi, 1, 'X slice')
+    warped_ivz_grids = ImageViewer3D_Sliced_Grids( ax[2,2], warped, phi, 2, 'Y slice')
+
+feh = viewers.FigureEventHandler(fig)
+
+feh.add_axes_event('button_press_event', ax[0,0], ivx.on_mouse_press)
+feh.add_axes_event('button_press_event', ax[0,1], ivy.on_mouse_press)
+feh.add_axes_event('button_press_event', ax[0,2], ivz.on_mouse_press)
+
+feh.add_axes_event('button_press_event', ax[1,0], warped_ivx.on_mouse_press)
+feh.add_axes_event('button_press_event', ax[1,1], warped_ivy.on_mouse_press)
+feh.add_axes_event('button_press_event', ax[1,2], warped_ivz.on_mouse_press)
+
+feh.add_axes_event('button_press_event', ax[2,0], warped_ivx_grids.on_mouse_press)
+feh.add_axes_event('button_press_event', ax[2,1], warped_ivy_grids.on_mouse_press)
+feh.add_axes_event('button_press_event', ax[2,2], warped_ivz_grids.on_mouse_press)
+
+feh.synchronize([ax[0,0], ax[0,1], ax[0,2], ax[1,0], ax[1,1], ax[1,2],ax[2,0], ax[2,1], ax[2,2]])
+
+plt.show()
+# plt.savefig("./data/imageViewer.png")
